4c6277b584c6dc538e07697b1e6e29fa088d6227
YeonGyeong <iamyeongyeong@gmail.com>
2024-01-25 14:35:26 +0900

    첫번째 커밋 계산기 시작 더하기 함수 완성

---
+++ main.py
@@ -0,0 +1,16 @@
+## 함수 선언부
+def add_func(n1, n2) :
+    retVal=n1 + n2
+    return retVal
+
+
+## 전역 변수부
+num1, num2, res = 100, 200, 0
+#int num1=100
+#int num2=200
+#int res=0
+
+
+## 메인 코드부
+res = add_func(num1, num2)
+print(num1,'+',num2,'=',res)
